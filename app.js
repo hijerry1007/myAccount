584852d9af2b85f10bb15df4e529cd6bbcd25477
Jerry Lai <jerrydavid123@hotmail.com>
2020-03-14 10:31:37 +0800

    Install mongoose and connect mongodb

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/account', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected')
+})
 
 app.use('/', require('./routes/home'))
-app.use('/accounts', require('./routes/account'))
+app.use('/account', require('./routes/accounts'))
 
 
 app.listen(3000, () => {
