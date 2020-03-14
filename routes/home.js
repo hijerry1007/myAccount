c96e840453d626378f933e8f4b53fd58dac60631
Jerry Lai <jerrydavid123@hotmail.com>
2020-03-14 10:52:46 +0800

    rectify error

--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const Record = require('.models/record')
+const Record = require('./models/record')
 // model
 
 router.get('/', (req, res) => {
