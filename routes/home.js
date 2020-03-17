7e51fc1e2012e1a5dd7a41bb48a18153d667c925
Jerry Lai <jerrydavid123@hotmail.com>
2020-03-17 20:50:56 +0800

    Finish date filter and update home route

--- routes/home.js
+++ routes/home.js
@@ -12,21 +12,80 @@ const { authenticated } = require('../config/auth')
 
 // home page
 router.get('/', authenticated, (req, res) => {
-  Record.find({ userId: req.user._id })
-    .lean()
-    .exec((err, records) => {
-      if (err) return console.error(err)
+  console.log(req.query)
+  let { dateBeg, dateEnd, selectedCategory } = req.query
+  let errors = []
+  const filter_category = (selectedCategory === 'all') ? {} : { category: selectedCategory }
+  const filterTime = {
+    date: {
+      $gte: dateBeg, $lte: dateEnd
+    }
+  }
+  //有時間有類別
+  if (dateBeg !== '' && dateEnd !== '' && selectedCategory !== '') {
 
-      let totalAmount = 0
-      for (let i = 0; i < records.length; i++) {
-        totalAmount += records[i].amount
-      }
-      return res.render('index', { records: records, totalAmount: totalAmount, category: category, month: month, year: year })
-    })
+    Record.find({ userId: req.user._id })
+      .find({ filter_category })
+      .find({ filterTime })
+      .lean()
+      .exec((err, records) => {
+        if (err) return console.error(err)
+        let totalAmount = 0
+        for (let i = 0; i < records.length; i++) {
+          totalAmount += records[i].amount
+        }
+        return res.render('index', { records: records, totalAmount: totalAmount, category: category, month: month, year: year, errors })
+      })
+  }
+  else if (dateBeg === '' && dateEnd === '' && selectedCategory !== '') {
+    //有類別沒時間
+    Record.find({ userId: req.user._id })
+      .find({ filter_category })
+      .lean()
+      .exec((err, records) => {
+        if (err) return console.error(err)
+        let totalAmount = 0
+        for (let i = 0; i < records.length; i++) {
+          totalAmount += records[i].amount
+        }
+        return res.render('index', { records: records, totalAmount: totalAmount, category: category, month: month, year: year, errors })
+      })
+  }
+  else if ((dateBeg === '' && dateEnd !== '') || (dateBeg !== '' && dateEnd === '')) {
+    //錯誤情形
+    errors.push({ message: '請選擇要搜尋的起始及結束的時間' })
+    Record
+      .find({ userId: req.user._id })
+      .find({ filter_category })
+      .lean()
+      .exec((err, records) => {
+        if (err) return console.error(err)
+        let totalAmount = 0
+        for (let i = 0; i < records.length; i++) {
+          totalAmount += records[i].amount
+        }
+        return res.render('index', { records: records, totalAmount: totalAmount, category: category, month: month, year: year, errors })
+      })
+  }
+  else {
+
+    Record
+      .find({ userId: req.user._id })
+      .lean()
+      .exec((err, records) => {
+        if (err) return console.error(err)
+        let totalAmount = 0
+        for (let i = 0; i < records.length; i++) {
+          totalAmount += records[i].amount
+        }
+        return res.render('index', { records: records, totalAmount: totalAmount, category: category, month: month, year: year })
+      })
+  }
 })
 
 //filter
 router.post('/filter', authenticated, (req, res) => {
+  console.log(req.query)
   let { selectedYear, selectedMonth, selectedCategory } = req.body
   let errors = []
   if (selectedYear === 'all' && selectedMonth !== "all") {
